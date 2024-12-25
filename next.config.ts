74b3415631bd21108a5498deb03f9692b3dee9cc
navid-kamal <navidkamal@iut-dhaka.edu>
2024-12-25 12:16:08 +0600

    add antialised

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    images: {
+        // Example: https://a.espncdn.com/i/teamlogos/ncaa/500/66.png
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'a.espncdn.com',
+                port: '',
+                pathname: '/i/teamlogos/ncaa/**',
+            },
+        ],
+        formats: ['image/avif', 'image/webp'],
+    },
+    experimental: {
+        // appDir: true,
+    },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,75 @@
-import Image from "next/image";
+import Image from 'next/image';
 
-export default function Home() {
+function Row({
+               image,
+               name,
+               score,
+               win,
+             }: {
+  image: string;
+  name: string;
+  score: string;
+  win?: boolean;
+}) {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
+      <div className="px-4 py-2 flex justify-between border-b border-gray-200">
+        <div className="flex">
+          <Image
+              src={image}
+              alt={name}
               width={20}
               height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+              className="h-6 w-6"
+          />
+          <p className="font-semibold ml-2">{name}</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+        <div className="flex text-right">
+          <p className="text-gray-700">{score}</p>
+          {win ? (
+              <p className="text-green-700 font-semibold ml-2 w-3">W</p>
+          ) : (
+              <p className="text-red-700 font-semibold ml-2 w-3">L</p>
+          )}
+        </div>
+      </div>
+  );
+}
+export default function Page() {
+  return (
+      <div className="">
+        <h2 className="font-semibold text-2xl mx-2 my-2">Schedule</h2>
+        <h3 className="font-semibold text-lg mx-2 mt-2">Next</h3>
+        <Row
+            image="https://a.espncdn.com/i/teamlogos/ncaa/500/66.png"
+            name="IUPUI"
+            score="89-39"
+            win
+        />
+        <h3 className="font-semibold text-lg mx-2 mt-2">Full</h3>
+        <div>
+          <Row
+              image="https://a.espncdn.com/i/teamlogos/ncaa/500/66.png"
+              name="IUPUI"
+              score="89-39"
+              win
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+          <Row
+              image="https://a.espncdn.com/i/teamlogos/ncaa/500/66.png"
+              name="IUPUI"
+              score="89-39"
+              win
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
+          <Row
+              image="https://a.espncdn.com/i/teamlogos/ncaa/500/66.png"
+              name="Iowa State Cyclones"
+              score="89-39"
           />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <Row
+              image="https://a.espncdn.com/i/teamlogos/ncaa/500/66.png"
+              name="IUPUI"
+              score="102-22"
+          />
+        </div>
+      </div>
   );
 }
